ecab1381b1b12c64c07522c22bb5df87bd774773
Flavio Deroo <flavio.deroo@emma-sleep.com>
2024-01-02 11:31:13 +0100

    draft

---
+++ firefox/popup.js
@@ -0,0 +1,26 @@
+document.addEventListener('DOMContentLoaded', function () {
+    var goButton = document.getElementById('goButton');
+    var urlInput = document.getElementById('urlInput');
+
+    function openOrderSearch() {
+        var inputQuery = urlInput.value.trim();
+
+        if (inputQuery !== '') {
+            // Construct the Fluent Order URL
+            var fluentOrderURL = 'https://emmasleep.apps.fluentcommerce.com/oms/#/orders?orders_ref=%25' + encodeURIComponent(inputQuery) + '%25';
+
+            // Open a new tab with the constructed order URL
+            browser.tabs.create({
+                url: fluentOrderURL
+            });
+        }
+    }
+
+    goButton.addEventListener('click', openOrderSearch);
+
+    urlInput.addEventListener('keyup', function (event) {
+        if (event.key === 'Enter') {
+            openOrderSearch();
+        }
+    });
+});
